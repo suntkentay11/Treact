45b1e84f08e99ebaaa844818afe08275e775e183
suntkentay@gmail.com <suntkentay@gmail.com>
2026-03-15 15:04:21 -0500

    final

---
+++ menu.js
@@ -0,0 +1,7 @@
+function openMenu() {
+  document.querySelector(".menu").classList.add("menu--open");
+}
+
+function closeMenu() {
+  document.querySelector(".menu").classList.remove("menu--open");
+}
